d4ad0c72fe9c92908a308ab73b16df16ffac80b5
Stanley Lee <stwc@itu.dk>
2025-05-21 14:32:54 +0200

    Clustered Scatterplot

--- index.js
+++ index.js
@@ -1,16 +1,126 @@
-var width = 400;
-var height = 400;
+var d3;
+var data = "global_bleaching_environmental.csv";
+var w = 500;
+var h = 500;
 
-var myCanvas = d3
-    .select("#myVis")
+d3.csv(data).then(function (dataset) {
+  // Convert and filter data
+  dataset = dataset
+    .map(function (d) {
+      d.TSA_DHW = +d.TSA_DHW;
+      d.Percent_Bleaching = +d.Percent_Bleaching;
+      d.Date_Year = +d.Date_Year;
+      return d;
+    })
+    .filter(function (d) {
+      return !isNaN(d.TSA_DHW) && !isNaN(d.Percent_Bleaching);
+    });
+
+  data = dataset;
+  buildIt();
+});
+
+
+function buildIt() {
+  var svg = d3
+    .select(".container")
     .append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .style("background-color", "antiquewhite");
+    .attr("width", w)
+    .attr("height", h);
 
-myCanvas
+  var minX = d3.min(data, function (d) {
+    return d.TSA_DHW;
+  });
+  var maxX = d3.max(data, function (d) {
+    return d.TSA_DHW;
+  });
+  var minY = d3.min(data, function (d) {
+    return d.Percent_Bleaching;
+  });
+  var maxY = d3.max(data, function (d) {
+    return d.Percent_Bleaching;
+  });
+
+  var xScale = d3.scaleLinear().domain([minX, maxX]).range([0, w]).nice();
+
+  var yScale = d3.scaleLinear().domain([minY, maxY]).range([h, 0]).nice();
+
+  var x_axis = d3.axisBottom().scale(xScale);
+  var y_axis = d3.axisLeft().scale(yScale);
+
+  /* ===== circles ===== */
+
+  svg
+    .selectAll("circle")
+    .data(data)
+    .enter()
     .append("circle")
-    .attr("cx", width / 2)
-    .attr("cy", height / 2)
-    .attr("r", 20)
-    .attr("fill", "hsb(200, 100, 10)");
+    .attr("cx", function (d) {
+      return xScale(d.TSA_DHW);
+      // return xPositioning(d);
+    })
+    .attr("cy", function (d) {
+      return yScale(d.Percent_Bleaching);
+      // return yPositioning(d)
+    })
+    .attr("r", function (d) {
+      return circleCenter(d) * 8;
+    })
+    .attr("fill", "#2962FF");
+
+  /* ===== radii ===== */
+
+  function circleCenter(d) {
+    return Math.sqrt(d.Percent_Bleaching);
+  }
+
+  /* ===== year labels ===== */
+
+  svg
+    .selectAll("text")
+    .data(data)
+    .enter()
+    .append("text")
+    .text(function (d) {
+      return d.Date_Year;
+    })
+    .attr("text-anchor", "middle")
+    .attr("x", function (d) {
+      return xScale(d.TSA_DHW);
+    })
+    .attr("y", function (d) {
+      return yScale(d.Percent_Bleaching) + 3;
+    })
+    .attr("font-size", "12")
+    .attr("fill", "white");
+
+  /* ===== average ticket prices labels ===== */
+
+  svg
+    .append("g")
+    .selectAll("text")
+    .data(data)
+    .enter()
+    .append("text")
+    .text(function (d) {
+      return "This is bleach percentage" + d.Percent_Bleaching + "%";
+    })
+    .attr("text-anchor", "middle")
+    .attr("x", function (d) {
+      return xScale(d.TSA_DHW) + circleCenter(d) * 9;
+    })
+    .attr("y", function (d) {
+      return yScale(d.Percent_Bleaching) - circleCenter(d) * 8;
+    })
+    .attr("font-size", "11")
+    .attr("fill", "#546c78");
+
+  /* ===== axes ===== */
+
+  svg
+    .append("g")
+    .call(x_axis)
+    .attr("transform", "translate(0," + h + ")");
+
+  svg.append("g").call(y_axis);
+}
